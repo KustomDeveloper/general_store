c1a9c00218d4da6595116a4c4c76b6adc856cbd1
MICHAEL HICKS <KUSTOMDESIGNER@GMAIL.COM>
2024-08-20 18:10:14 -0500

    first commit

---
+++ config/tailwind.config.js
@@ -0,0 +1,48 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    // colors: {
+    //   'blue': '#1fb6ff',
+    //   'pink': '#ff49db',
+    //   'orange': '#ff7849',
+    //   'green': '#13ce66',
+    //   'gray-dark': '#273444',
+    //   'gray': '#8492a6',
+    //   'gray-light': '#d3dce6',
+    // },
+    // fontFamily: {
+    //   sans: ['Graphik', 'sans-serif'],
+    //   serif: ['Merriweather', 'serif'],
+    // },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
